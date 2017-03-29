ff79313cce10f98b2542a6b5c5a051a09f3acdce
cinly <cinlyma@gmail.com>
2017-03-29 13:53:55 +0800

    use simple_format helper to decorate

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
-  before_action :find_group_and_check_permission, only: [:edie, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
   end
@@ -75,7 +75,7 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
 
      if current_user != @group.user
-       redirect_to root_path, alert: "You are no permission."
+       redirect_to root_path, alert: "You have no permission."
      end
   end
 
